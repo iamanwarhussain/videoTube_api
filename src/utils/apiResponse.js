99f641e9c8f2721d4a74d1245ef4b10f27a82a57
iamanwarhussain <iamanwarhussain60006@gmail.com>
2026-06-13 00:15:13 +0530

    feat: implement apiResponse class for standardized API responses

---
+++ src/utils/apiResponse.js
@@ -0,0 +1,10 @@
+class apiResponse {
+    constructor(statusCode, data, message = "success", success = true){
+        this.statusCode = statusCode,
+        this.data = data,
+        this.message = message,
+        this.success = success
+    }
+}
+
+module.exports = apiResponse
