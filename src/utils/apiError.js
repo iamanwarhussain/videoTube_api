eda99f62f6874fdb88d7335f63cea399e39d7ddc
iamanwarhussain <iamanwarhussain60006@gmail.com>
2026-06-13 00:24:59 +0530

    feat: implement apiError class for standardized error handling and refactor apiResponse constructor

---
+++ src/utils/apiError.js
@@ -0,0 +1,18 @@
+class apiError extends Error {
+    constructor(statusCode, message = "Something went wrong.", error = [], stack = ""){
+        super(message)
+        this.statusCode = statusCode
+        this.message = message
+        this.error = error
+        this.success = false
+
+        if(stack){
+            this.stack = stack
+        }
+        else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+module.exports = apiError
--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -1,8 +1,8 @@
 class apiResponse {
     constructor(statusCode, data, message = "success", success = true){
-        this.statusCode = statusCode,
-        this.data = data,
-        this.message = message,
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
         this.success = success
     }
 }
