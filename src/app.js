62e8c8d09d458e1344062b6ca3bce40fac980cfc
iamanwarhussain <iamanwarhussain60006@gmail.com>
2026-06-13 07:57:41 +0530

    feat: add morgan logging middleware for structured log messages

--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
+const morgan = require("morgan")
+const logger = require("./utils/logger.js")
 
 const app = express()
+
+// Logging format and logger middleware for structerized log messages
+const morganFormat = ":method :url :status :response-time ms"
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 // Build-in middlewares
 app.use(cors({ origin : process.env.CORS , credentials : true }))
