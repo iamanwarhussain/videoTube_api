e1aca3e40ba94eb8f1b078141a72566077225124
iamanwarhussain <iamanwarhussain60006@gmail.com>
2026-06-13 11:22:11 +0530

    fix: update dotenv configuration to specify path for environment variables

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-const dotenv = require("dotenv").config()
+const path = require("path")
+require("dotenv").config({path : path.resolve(__dirname, ".env")})
 const app = require("./app.js")
 const ConnectDB = require("../src/config/index.js")
 
