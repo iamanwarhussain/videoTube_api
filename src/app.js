3444415533dc6ae6945d4ee2392513b14e02acf1
iamanwarhussain <iamanwarhussain60006@gmail.com>
2026-06-13 11:22:24 +0530

    fix: correct cookieParser usage and update health check route path

--- src/app.js
+++ src/app.js
@@ -27,15 +27,15 @@ app.use(
 
 // Build-in middlewares
 app.use(cors({ origin : process.env.CORS , credentials : true }))
-app.use(cookieParser)
+app.use(cookieParser())
 app.use(express.json({ limit : "16kb" }))
-app.use(express.urlencoded({ limit : "16kb" }))
+app.use(express.urlencoded({ extended : true , limit : "16kb" }))
 app.use(express.static("public"))
 
 // Importing routes
 const healthCheckRoute = require("./routes/healthcheck.route.js")
 
 // Using routes
-app.use("healthcheck", healthCheckRoute)
+app.use("/", healthCheckRoute)
 
 module.exports = app
