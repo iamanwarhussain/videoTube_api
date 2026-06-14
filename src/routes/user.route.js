a6e981a1bf2fe1772fc8d665f9bd590aadac7ff9
iamanwarhussain <iamanwarhussain60006@gmail.com>
2026-06-14 09:28:43 +0530

    feat: add user authentication routes for login, logout, and profile management

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,18 @@
-const {registerUser} = require("../controllers/user.controller.js")
+const {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changePassword,
+    changeProfile,
+    changeCoverImage,
+    updateDetails,
+    getUserProfile,
+    getUserHistory
+} = require("../controllers/user.controller.js")
 const { Router } = require("express")
 const upload = require("../middlewares/multer.middleware.js")
+const verifyToken = require("../middlewares/auth.middleware.js")
 
 const router = Router()
 
@@ -17,5 +29,21 @@ router.route("/register").post(
     ]),
     registerUser
 )
+
+router.route("/login").post(loginUser)
+
+router.route("/logout").post(verifyToken, logoutUser)
+
+router.route("/refresh-access-token").post(refreshAccessToken)
+
+router.route("/password").post(verifyToken, changePassword)
+
+router.route("/profile/img").post(verifyToken, upload.single("profileImg"), changeProfile)
+
+router.route("/cover/img").post(verifyToken, upload.single("coverImage"), changeCoverImage)
+
+router.route("/update").post(verifyToken, updateDetails)
+
+
 
 module.exports = router
